96506a82f12dcddd467381a72514ca46ae1928d6
RafaelDaSilva1984 <rfsrafaelsilva@gmail.com>
2026-01-13 15:36:50 -0300

    Update wordcloud image and improve generator script

--- rafael.python.nuvem.palavras/gerar_wordcloud.py
+++ rafael.python.nuvem.palavras/gerar_wordcloud.py
@@ -1,24 +1,25 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-texto = """
-Python Python SQL SQL
-Backend APIs REST
-Data Analysis Pandas NumPy
-ETL Data Pipelines
-Web Scraping Automation
-Power BI BI Analytics
-Excel Data Automation
-Git GitHub Version Control
-"""
 
+# Abre o arquivo de texto e lê todo o conteúdo
+# encoding="utf-8" evita problemas com acentos
+with open("tecnologias.txt", encoding="utf-8") as arquivo:
+    texto = arquivo.read()
+
+
+# Cria a nuvem de palavras com configurações visuais
 wc = WordCloud(
     width=1000, height=500, background_color="black", colormap="viridis"
 ).generate(texto)
 
+
+# Exibe a nuvem de palavras
 plt.figure(figsize=(12, 6))
 plt.imshow(wc, interpolation="bilinear")
 plt.axis("off")
 plt.show()
 
+
+# Salva a imagem gerada em arquivo PNG
 wc.to_file("wordcloud.png")
